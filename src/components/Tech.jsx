4317f67a77741896ed1689a5abda20596ffaa96f
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2023-03-06 19:24:36 +0530

    working on projects component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { BallCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from "../constants"
+import { motion } from "framer-motion"
+import { fadeIn, textVariant } from "../utils/motion"
+import { styles } from '../styles'
+
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+    <motion.div
+    variants={textVariant()}
+    >
+      <p className={styles.sectionSubText}>What i know</p>
+      <h2 className={styles.sectionHeadText}>Technologies.</h2>
+    </motion.div>
+    <div className='flex mt-16 flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name
+        }>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
