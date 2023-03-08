cbed53dc840ccba80d761f2a88a6ca807beabf61
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2023-03-08 13:52:18 +0530

    done with stars and emailjs configuration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
+// oj8wLb1fMXjKnJORd
+// template_1r3mq7m
+// service_eub7lza
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,8 +20,40 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
-  const handleSubmit = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value})
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send('service_eub7lza',
+    'template_1r3mq7m',
+    {
+      from_name: form.name,
+      to_name: "Harsh Varmora",
+      from_email: form.email,
+      to_email: 'harshvarmora28@gmail.com',
+      message: form.message,
+    },
+    'oj8wLb1fMXjKnJORd'
+    ).then(() => {
+      setLoading(false);
+      alert("Thank you. I will get back to you as soon as possible.");
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false);
+      console.log(error);
+      alert("Somethin went wrong.");
+    })
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
